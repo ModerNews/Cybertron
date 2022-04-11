e0e720af6b13033af27739495c32f82a4da474c7
ModerNews <grzesiujag@poczta.fm>
2022-04-11 10:52:28 +0200

    file upload part 1

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+import aiofiles
 
 app = FastAPI()
 app.mount('/static', StaticFiles(directory="static"), name="static")
@@ -12,6 +15,18 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/file")
+async def say_hello():
+    with open(".\\templates\\test_template.html", "r") as content:
+        return HTMLResponse(content.read())
+
+
+@app.post("/uploads")
+async def create_file(request: Request):
+    form = await request.form()
+    print(form)
+    filename = form["file"].filename
+    contents = await form["file"].read()
+    with open(filename, 'wb') as f:
+        f.write(contents)
+    return filename
