de8dd4d87d240a48ed70e0d783d5c3b269821fd4
ModerNews <grzesiujag@poczta.fm>
2022-04-11 12:40:13 +0200

    Mysql injection patch

--- crud.py
+++ crud.py
@@ -5,5 +5,7 @@ from typing import Optional
 
 def get_episodes(con: Connection, title: Optional[str]) -> list[tuple]:
     with con.cursor() as cur:
-        cur.execute(f'SELECT * FROM Episodes{f" WHERE serie_id = {title}" if title else ""}')
+        query = f'SELECT * FROM Episodes{f" WHERE serie_id = {title}" if title else ""}'
+        print(query)
+        cur.execute(query)
         return list(cur.fetchall())
